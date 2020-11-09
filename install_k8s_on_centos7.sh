1ca8446c86fc55eec3c724be26b0d3122eba31c7
wangmingco <45085989+wangmingco@users.noreply.github.com>
2020-11-09 15:18:52 +0800

    Update install_k8s_on_centos7.sh

--- install_k8s_on_centos7.sh
+++ install_k8s_on_centos7.sh
@@ -13,7 +13,7 @@ function config_firewall() {
 
 	setenforce 0
 
-	sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+	sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 	
 	cat /etc/selinux/config
 
